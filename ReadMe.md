de0de813d6937c9d957c4e8dc1df40aaa37a8127
RajenderDandyal <rajender0184@gmail.com>
2019-03-14 21:25:51 +0530

    updated

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+
+UseFull function to track changes in an object and runs callback when ever something changes in object
+It has no external dependencies, and written in pure js
+
+use cases:
+1. similar to redux store or react state, whenever something changes in object do something in callback ex sending data to server
+2. triger other events based on object property change, as we can track which property of object changes and responds accordingly
--- testing.js
+++ testing.js
@@ -1,4 +1,4 @@
-//import onChange from "./onChange";
+
 let onChange = require("./onChange");
 
 let obj = {a: 1, b: 2, c: 3, d: 4};
@@ -10,6 +10,10 @@ let objChanged = onChange(obj, function (path, value, previousValue) {
   console.log('value:', value);
   console.log('previousValue:', previousValue);
 });
+
+//obj.c = "i have changed c property";
+// now work on objChanged to track changes on obj
+
 objChanged.a = [1, 2, 4, 36, 4, 5];
-console.log(obj.a)
+objChanged.b = {apple: 1, mango: 3};
 
